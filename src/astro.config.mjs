9b4f076732d6a1e66002f6fbc8ab86c06cd44678
Denis Yudin <dyudin@intermedia.com>
2025-07-26 16:44:15 +0100

    chore(ci): Update deployment configuration and base path for production

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: process.env.NODE_ENV === 'production' ? 'https://dyudin0821.github.io' : 'http://localhost:4321',
-  base: process.env.NODE_ENV === 'production' ? '/portogate' : undefined,
+  base: process.env.NODE_ENV === 'production' ? '/portogate-site' : undefined,
   trailingSlash: 'ignore',
   build: {
     format: 'directory'
